525cb6fcf10b6b94f862f23ef7112d5375fb6849
xuedabao0424 <15010330571@163.com>
2018-04-09 22:20:28 +0800

    增加头部动画

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,9 +4,13 @@ import HomeHeader from "./HomeHeader";
 
 
 export default class Home extends React.Component{
+    //选择当前是哪门课程
+    selectCurrentLesson = (val)=>{//val是当前选择的课程
+
+    };
     render(){
         return <div>
-            <HomeHeader/>
+            <HomeHeader selectCurrentLesson={this.selectCurrentLesson}/>
         </div>
     }
 }
--- src/containers/Home/HomeHeader.js
+++ src/containers/Home/HomeHeader.js
@@ -1,5 +1,16 @@
 import React,{Component} from 'react';
 import logo from '../../common/images/logo.jpg';
+import Transition from 'react-transition-group/Transition';
+const duration=300;//动画时间
+const defaultStyle={
+    transition: `opacity ${duration}ms ease-in-out`,
+    opacity:0,
+    display:'none'
+};
+const transitionStyles={
+    entering:{opacity:0},
+    entered:{opacity:1}
+};
 export default class HomeHeader extends Component{
     constructor(){
         super();
@@ -17,11 +28,23 @@ export default class HomeHeader extends Component{
                         {this.state.isShow ? <i className="iconfont icon-guanbi"></i> : <i className="iconfont icon-liebiao"></i>}
                     </div>
                 </div>
-                <ul className="header-menu">
-                    <li>全部</li>
-                    <li>react</li>
-                    <li>vue</li>
-                </ul>
+                <Transition in={this.state.isShow} timeout={duration} onEnter={(node)=>{node.style.display='block'}}  onExited={(node)=>{node.style.display='none'}}>
+                    {(state)=>(
+                        <ul className="header-menu" style={{
+                            ...defaultStyle,
+                            ...transitionStyles[state]
+                        }}
+                            onClick={(e)=>{
+                                this.props.selectCurrentLesson(e.target.dataset.type);
+                                this.changeShow();//点完后  隐藏掉列表
+                            }}
+                        >
+                            <li data-type="all">全部</li>
+                            <li data-type="react">react课程</li>
+                            <li data-type="vue">vue课程</li>
+                        </ul>
+                    )}
+                </Transition>
             </div>
         )
     }
